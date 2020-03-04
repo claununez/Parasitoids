6ec3696ce7ac041cddd67186c7334e6e7b97fcc4
claununez <claununez199o@gmail.com>
2020-03-03 23:19:14 -0600

    Changes after first review

--- R_scripts/Statistical_Analyses_review.R
+++ R_scripts/Statistical_Analyses_review.R
@@ -35,52 +35,52 @@ paras_clean <- paras[-outs, ]
 
 
 # Normality test (Shapiro-Wilk's test)
-n_test <- apply(paras_clean[, c("Wing.length.(mm)", "Weight.(mg)")], 2,
+n_test <- apply(paras_clean[, c("Wing.length..mm.", "Weight..mg.")], 2,
                 shapiro.test)
 
 # t-test between countries
 Ecuador <- paras_clean[paras_clean$Country == 1, ]
 Venezuela <- paras_clean[paras_clean$Country == 2, ]
-WL_t.test <- t.test(Ecuador$Wing.length.(mm),
-                    Venezuela$Wing.length.(mm), alternative = "less")
+WL_t.test <- t.test(Ecuador$Wing.length..mm.,
+                    Venezuela$Wing.length..mm., alternative = "less")
 
-WE_t.test <- t.test(Ecuador$Weight.(mg), Venezuela$Weight.(mg),
+WE_t.test <- t.test(Ecuador$Weight..mg., Venezuela$Weight..mg.,
                     alternative = "less")
 
 # t-test between sex
 Female <- paras_clean[paras_clean$Sex == 1, ]
 Male <- paras_clean[paras_clean$Sex == 2, ]
-WL_Sex_t.test <- t.test(Female$Wing.length.(mm), Male$Wing.length.(mm),
+WL_Sex_t.test <- t.test(Female$Wing.length..mm., Male$Wing.length..mm.,
                         alternative = "less")
 
-WE_Sex_t.test <- t.test(Female$Weight.(mg), Male$Weight.(mg),
+WE_Sex_t.test <- t.test(Female$Weight..mg., Male$Weight..mg.,
                         alternative = "less")
 
 # t-test between strategy
 Idio <- paras_clean[paras_clean$Strategy == "idiobiont", ]
 Koin <- paras_clean[paras_clean$Strategy == "koinobiont", ]
-WL_Str_t.test <- t.test(Idio$Wing.length.(mm), Koin$Wing.length.(mm),
+WL_Str_t.test <- t.test(Idio$Wing.length..mm., Koin$Wing.length..mm.,
                         alternative = "less")
 
-WE_Str_t.test <- t.test(Idio$Weight.(mg), Koin$Weight.(mg),
+WE_Str_t.test <- t.test(Idio$Weight..mg., Koin$Weight..mg.,
                         alternative = "less")
 
 
 # Boxplot representation
 par(cex = 0.73, mar = c(4, 4, 0, 0), mfrow = c(2, 3))
 
-boxplot(paras_clean$Weight.(mg) ~ paras_clean$Country, xlab = "",
+boxplot(paras_clean$Weight..mg. ~ paras_clean$Country, xlab = "",
         ylab = "Mass (mg)", las = 1, names = c("Ecuador", "Venezuela") )
-boxplot(paras_clean$Weight.(mg)~paras_clean$Sex, xlab = "",
+boxplot(paras_clean$Weight..mg.~paras_clean$Sex, xlab = "",
         ylab = "", las = 1, names = c("Female","Male"))
-boxplot(paras_clean$Weight.(mg)~paras_clean$Strategy, xlab = "",
+boxplot(paras_clean$Weight..mg.~paras_clean$Strategy, xlab = "",
         ylab = "", las = 1, names = c("Koinobiont", "Idiobiont"))
 
-boxplot(paras_clean$Wing.length.(mm) ~ paras_clean$Country, xlab = "Country",
+boxplot(paras_clean$Wing.length..mm. ~ paras_clean$Country, xlab = "Country",
         ylab = "Wing length (mm)", las = 1, names = c("Ecuador", "Venezuela") )
-boxplot(paras_clean$Wing.length.(mm)~paras_clean$Sex, xlab = "Sex",
+boxplot(paras_clean$Wing.length..mm.~paras_clean$Sex, xlab = "Sex",
         ylab = "", las = 1,names = c("Female", "Male") )
-boxplot(paras_clean$Wing.length.(mm)~paras_clean$Strategy, xlab = "Strategy",
+boxplot(paras_clean$Wing.length..mm.~paras_clean$Strategy, xlab = "Strategy",
         ylab = "", las = 1, names = c("Koinobiont", "Idiobiont"))
 
 
@@ -98,26 +98,70 @@ legend("bottomright", c("Female", "Male"), pch = pchs,
 legend("topleft", legend = "(a) Overall relationship", bty = "n", cex = 1.2)
 
 par(cex = 0.7, mar = c(4, 4, 0, 0))
-boxplot(paras_clean$Weight.(mg) ~ paras_clean$Country, xlab = "",
+boxplot(paras_clean$Weight..mg. ~ paras_clean$Country, xlab = "",
         ylab = "Mass (mg)", las = 1, names = c("Ecuador", "Venezuela") )
 legend("topright", legend = "(b)", bty = "n", cex = 1.2)
-boxplot(paras_clean$Weight.(mg)~paras_clean$Sex, xlab = "",
+boxplot(paras_clean$Weight..mg.~paras_clean$Sex, xlab = "",
         ylab = "", las = 1, names = c("Female","Male"))
 legend("topright", legend = "(c)", bty = "n", cex = 1.2)
-boxplot(paras_clean$Weight.(mg)~paras_clean$Strategy, xlab = "",
+boxplot(paras_clean$Weight..mg.~paras_clean$Strategy, xlab = "",
         ylab = "", las = 1, names = c("Koinobiont", "Idiobiont"))
 legend("topright", legend = "(d)", bty = "n", cex = 1.2)
 
-boxplot(paras_clean$Wing.length.(mm) ~ paras_clean$Country, xlab = "Country",
+boxplot(paras_clean$Wing.length..mm. ~ paras_clean$Country, xlab = "Country",
         ylab = "Wing length (mm)", las = 1, names = c("Ecuador", "Venezuela") )
 legend("topright", legend = "(e)", bty = "n", cex = 1.2)
-boxplot(paras_clean$Wing.length.(mm)~paras_clean$Sex, xlab = "Sex",
+boxplot(paras_clean$Wing.length..mm.~paras_clean$Sex, xlab = "Sex",
         ylab = "", las = 1,names = c("Female", "Male") )
 legend("topright", legend = "(f)", bty = "n", cex = 1.2)
-boxplot(paras_clean$Wing.length.(mm)~paras_clean$Strategy, xlab = "Strategy",
+boxplot(paras_clean$Wing.length..mm.~paras_clean$Strategy, xlab = "Strategy",
         ylab = "", las = 1, names = c("Koinobiont", "Idiobiont"))
 legend("topright", legend = "(g)", bty = "n", cex = 1.2)
 dev.off()
+
+# Testing effect of country and sex in relationships 
+## Ancova Country (effects of elevation)
+paras_anc <- paras_clean
+paras_anc$Country <- as.factor(paras_anc$Country)
+
+### First model
+fit1 <- aov(Weight..mg. ~ Wing.length..mm. * Country, data = paras_anc)
+summary(fit1)
+
+### Second model
+fit2 <- aov(Weight..mg. ~ Wing.length..mm. + Country, data = paras_anc)
+summary(fit2)
+
+### Comparing the two models
+anova(fit1, fit2)
+
+## Ancova Sex (effects of sex on relationship)
+paras_anc$Sex <- as.factor(paras_anc$Sex)
+
+### First model
+fit3 <- aov(Weight..mg. ~ Wing.length..mm. * Sex, data = paras_anc)
+summary(fit3)
+
+### Second model
+fit4 <- aov(Weight..mg. ~ Wing.length..mm. + Sex, data = paras_anc)
+summary(fit4)
+
+### Comparing the two models
+anova(fit3, fit4)
+
+## Ancova Strategy 
+paras_anc$Strategy <- as.factor(paras_anc$Strategy)
+
+### First model
+fit5 <- aov(Weight..mg. ~ Wing.length..mm. * Strategy, data = paras_anc)
+summary(fit5)
+
+### Second model
+fit6 <- aov(Weight..mg. ~ Wing.length..mm. + Strategy, data = paras_anc)
+summary(fit6)
+
+### Comparing the two models
+anova(fit5, fit6)
 
 
 # Dividing the data in training and testing
@@ -125,62 +169,103 @@ set.seed(1)
 train_all <- paras_clean[sample(1:dim(paras_clean)[1], dim(paras_clean) * 0.75), ]
 train_female <- Female[sample(1:dim(Female)[1], dim(Female) * 0.75), ]
 train_male <- Male[sample(1:dim(Male)[1], dim(Male) * 0.75), ]
+train_idio <- Idio[sample(1:dim(Idio)[1], dim(Idio) * 0.75), ]
+train_koin <- Koin[sample(1:dim(Koin)[1], dim(Koin) * 0.75), ]
 
 test_all <- paras_clean[!row.names(paras_clean) %in% row.names(train_all), ]
 test_female <- Female[!row.names(Female) %in% row.names(train_female), ]
-test_male<- Male[!row.names(Male) %in% row.names(train_male), ]
+test_male <- Male[!row.names(Male) %in% row.names(train_male), ]
+test_idio <- Idio[!row.names(Idio) %in% row.names(train_idio), ]
+test_koin <- Koin[!row.names(Koin) %in% row.names(train_koin), ]
 
 
 # Regressions
 ## training
-regre_train_all <- lm(log(train_all$Weight.(mg)) ~
-                              log(train_all$Wing.length.(mm)))
+regre_train_all <- lm(log(train_all$Weight..mg.) ~
+                        log(train_all$Wing.length..mm.))
 summary(regre_train_all)
-regre_train_female <- lm(log(train_female$Weight.(mg)) ~
-                                 log(train_female$Wing.length.(mm)))
+regre_train_female <- lm(log(train_female$Weight..mg.) ~
+                           log(train_female$Wing.length..mm.))
 summary(regre_train_female)
-regre_train_male <- lm(log(train_male$Weight.(mg)) ~
-                               log(train_male$Wing.length.(mm)))
+regre_train_male <- lm(log(train_male$Weight..mg.) ~
+                         log(train_male$Wing.length..mm.))
 summary(regre_train_male)
+regre_train_idio <- lm(log(train_idio$Weight..mg.) ~
+                           log(train_idio$Wing.length..mm.))
+summary(regre_train_idio)
+regre_train_koin <- lm(log(train_koin$Weight..mg.) ~
+                         log(train_koin$Wing.length..mm.))
+summary(regre_train_koin)
 
 ## testing
 ### calling function
 source("https://raw.githubusercontent.com/claununez/Parasitoids/master/R_scripts/ichneumonidae_wwl.R")
 
 ### calculating weights with testing data and the function
-weight_all <- ichneumonidae_wwl(wing_length = test_all$Wing.length.(mm),
+weight_all <- ichneumonidae_wwl(wing_length = test_all$Wing.length..mm.,
                                 units_wing_length = "mm")
-weight_female <- ichneumonidae_wwl(wing_length = test_female$Wing.length.(mm),
-                                   units_wing_length = "mm", sex = "f")
-weight_male <- ichneumonidae_wwl(wing_length = test_male$Wing.length.(mm),
-                                 units_wing_length = "mm", sex = "m")
+weight_female <- ichneumonidae_wwl(wing_length = test_female$Wing.length..mm.,
+                                   units_wing_length = "mm", sex_or_strategy = "f")
+weight_male <- ichneumonidae_wwl(wing_length = test_male$Wing.length..mm.,
+                                 units_wing_length = "mm", sex_or_strategy = "m")
+weight_idio <- ichneumonidae_wwl(wing_length = test_idio$Wing.length..mm.,
+                                   units_wing_length = "mm", sex_or_strategy = "idi")
+weight_koin <- ichneumonidae_wwl(wing_length = test_koin$Wing.length..mm.,
+                                 units_wing_length = "mm", sex_or_strategy = "koi")
 
 ### correlation between calculated and measured testing weights
-weight_cor_test <- cor.test(weight_all, test_all$Weight.(mg))
-weight_cor_test_female <- cor.test(weight_female, test_female$Weight.(mg))
-weight_cor_test_male <- cor.test(weight_male, test_male$Weight.(mg))
+weight_cor_test <- cor.test(weight_all, test_all$Weight..mg.)
+weight_cor_test_female <- cor.test(weight_female, test_female$Weight..mg.)
+weight_cor_test_male <- cor.test(weight_male, test_male$Weight..mg.)
+weight_cor_test_idio <- cor.test(weight_idio, test_idio$Weight..mg.)
+weight_cor_test_koin <- cor.test(weight_koin, test_koin$Weight..mg.)
 
 
 # Plotting regressions
-jpeg(filename = "Wing_length_Weight_rel_summary1.jpg", width = 16.6, height = 8,
+strat <- paras_clean$Strategy
+colss <- c("orange", "brown")
+
+jpeg(filename = "Wing_length_Weight_rel_summary_review.jpg", width = 16.6, height = 16,
      units = "cm", res = 600)
-par(mar = c(4, 4.5, 0, 0), mfrow = c(1, 2))
+par(mar = c(4, 4, 0.5, 0.5), mfrow = c(2, 2))
 
 ## log Weight/Wing lengths
 par(cex = 0.7)
-plot(log(train_all$Wing.length.(mm)), log(train_all$Weight.(mg)),
+plot(log(train_all$Wing.length..mm.), log(train_all$Weight..mg.),
      pch = pchs[sex], col = cols[sex],
      ylab = "Natural logarithm of mass (mg)",
      xlab = "Natural logarithm of wing length (mm)", las = 1)
-abline(lm(log(train_all$Weight.(mg)) ~
-                  log(train_all$Wing.length.(mm)) ), lty = 1, lwd = 1, col = "black")
+abline(lm(log(train_all$Weight..mg.) ~
+            log(train_all$Wing.length..mm.) ), lty = 1, lwd = 1, col = "black")
 legend("topleft", legend = "(a)", bty = "n", cex = 1.2)
+legend("bottomright", c("Female", "Male"), pch = pchs,
+       col = cols, bg = "white", cex = 0.9, bty = "n")
+
+plot(log(train_all$Wing.length..mm.), log(train_all$Weight..mg.),
+     pch = pchs[strat], col = colss[strat],
+     ylab = "", xlab = "Natural logarithm of wing length (mm)", las = 1)
+abline(lm(log(train_all$Weight..mg.) ~
+            log(train_all$Wing.length..mm.) ), lty = 1, lwd = 1, col = "black")
+legend("topleft", legend = "(b)", bty = "n", cex = 1.2)
+legend("bottomright", c("Idiobiont", "Koinobiont"), pch = pchs,
+       col = colss, bg = "white", cex = 0.9, bty = "n")
 
 ## Calculated/Measured data
-plot(test_all$Weight.(mg), weight_all, ylab = "Calculated mass (mg)",
-     xlab = "Measured mass (mg)", las = 1)
-abline(lm(weight_all ~ test_all$Weight.(mg)), lty = 2, lwd = 1, col = "black")
-legend("topleft", legend = "(b)", bty = "n", cex = 1.2)
+xm <- max(max(weight_female), max(weight_male))
+ym <- max(max(test_male$Weight..mg), max(test_female$Weight..mg))
+plot(test_male$Weight..mg., weight_male, ylab = "Calculated mass (mg)", xlim = c(0, xm),
+     ylim = c(0, ym), xlab = "Measured mass (mg)", las = 1, col = cols[2], pch = pchs[2])
+points(test_female$Weight..mg., weight_female, col = cols[1], pch = pchs[1])
+abline(lm(weight_all ~ test_all$Weight..mg.), lty = 2, lwd = 1, col = "black")
+legend("topleft", legend = "(c)", bty = "n", cex = 1.2)
+
+xm <- max(max(weight_idio), max(weight_koin))
+ym <- max(max(test_idio$Weight..mg), max(test_koin$Weight..mg))
+plot(test_idio$Weight..mg., weight_idio, ylab = "", xlim = c(0, xm),
+     ylim = c(0, ym), xlab = "Measured mass (mg)", las = 1, col = colss[1], pch = pchs[1])
+points(test_koin$Weight..mg., weight_koin, col = colss[2], pch = pchs[2])
+abline(lm(weight_all ~ test_all$Weight..mg.), lty = 2, lwd = 1, col = "black")
+legend("topleft", legend = "(d)", bty = "n", cex = 1.2)
 
 dev.off()
 
@@ -193,22 +278,22 @@ subs <- c("Campopleginae", "Cryptinae", "Ichneumoninae", "Pimplinae", "Orthocent
 
 regs <- lapply(subs, function(x) {
   sub <- paras_clean[as.character(paras_clean$Subfamily) == x, ]
-  lm(log(sub$Weight.(mg)) ~ log(sub$Wing.length.(mm)))
+  lm(log(sub$Weight..mg.) ~ log(sub$Wing.length..mm.))
 })
 
 regs
 
 
 # plotting regresions
-lwl <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs, "Wing.length.(mm)"])
-lw <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs, "Weight.(mg)"])
+lwl <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs, "Wing.length..mm."])
+lw <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs, "Weight..mg."])
 cols <- c("#000000", "#A50F0F", "#0D2B94", "#2EB471", "#501C88", "#CC8D16",
           "#B721D5", "#22EA1E", "#795E20")
 
-lwlo <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[5], "Wing.length.(mm)"])
-lwo <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[5], "Weight.(mg)"])
-lwlm <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[8], "Wing.length.(mm)"])
-lwm <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[8], "Weight.(mg)"])
+lwlo <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[5], "Wing.length..mm."])
+lwo <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[5], "Weight..mg."])
+lwlm <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[8], "Wing.length..mm."])
+lwm <- log(paras_clean[as.character(paras_clean$Subfamily) %in% subs[8], "Weight..mg."])
 
 
 jpeg(filename = "Wing_length_Weight_rel_subfam1.jpg", width = 16.6, height = 8,
@@ -264,7 +349,7 @@ reg_res <- lapply(regs, function(x) {
              Residual_error = round(sum$sigma, 2),
              Equation_derived = paste0("lw = ", round(sum$coefficients[2, 1], 5),
                                        " x lwl - ", abs(round(sum$coefficients[1, 1], 5))))
-
+  
 })
 
 regtable <- rbind(data.frame(Data = c("Combined data", "Female data", "Male data"),
--- R_scripts/ichneumonidae_wwl.R
+++ R_scripts/ichneumonidae_wwl.R
@@ -1,49 +1,70 @@
-#' Calculate weight from wing length in Ichneumonidae insects
+#' Calculate body mass from wing length in Ichneumonidae insects
 #'
-#' @description ichneumonidae_wwl helps to estimate the weight of the insects 
-#' of Ichneumonidae family considering the length of the forewing.
+#' @description ichneumonidae_wwl helps to estimate the body mass of insects 
+#' of the Ichneumonidae family considering the length of the forewing.
 #'
-#' @param wing_length (numeric) Vector of wing length's measurements.
-#' @param units_wing_length (character) Units of the wing length measurements. Options are 
-#' millimeters = "mm", centimeters = "cm", or inches = "inch".
-#' @param sex (character) Sex of the individuals. Options are unknown = "unknown", 
-#' males = "m", or females = "f".
+#' @param wing_length (numeric) Vector of wing length measurements.
+#' @param units_wing_length (character) Units of the wing length measurements. 
+#' Options are millimeters = "mm", centimeters = "cm", or inches = "in".
+#' @param sex_or_strategy (character) Sex or parasitism strategy of the specimens.
+#' Options are: unknown = "unknown", males = "m", females = "f", 
+#' idiobiont = "idi", or koinobiont = "koi". Default = "unknown"; see details.
 #' 
 #'
 #' @return
-#' A vector of weight vales in milligrams (mg).
+#' A vector of weight values in milligrams (mg).
 #'
 #' @details 
-#' Equations used are as follow
-#' unknown sex:  y = exp(-2.40998 + (1.89329 * log(wing_length)))
+#' Citation:
+#' 
+#' To cite the use of this function in scientific papers use:
+#' 
+#' Mazón, M., C. Nuñez-Penichet, M.E. Cobos (in review). Relationship between 
+#' body mass and forewing length in Neotropical Ichneumonidae (Insecta: 
+#' Hymenoptera). Neotropical Entomology.
+#' 
+#' Calculations are made based on the equations described below. Argument 
+#' \code{sex_or_strategy} allows definitions of sex or parasitism strategy, but 
+#' not combinations of both categories. If "unknown" (the default) is defined,
+#' a formula obtaine for data combining information of a large list of specimens
+#' is used.
+#' 
+#' Equations used:
+#' 
+#' unknown:  y = exp(-2.40998 + (1.89329 * log(wing_length)))
 #' females: y = exp(-2.35497 + (1.87731 * log(wing_length))
 #' males:  y = exp(-2.48655 + (1.91227 * log(wing_length)))
+#' idiobiont: y = exp(-2.20067 + (1.81526 * log(wing_length))
+#' koinobiont:  y = exp(-2.41816 + (1.87424 * log(wing_length)))
 
-ichneumonidae_wwl <- function(wing_length, units_wing_length, sex = "unknown") {
+ichneumonidae_wwl <- function(wing_length, units_wing_length, 
+                              sex_or_strategy = "unknown") {
   
   if (missing(wing_length) ) {
-    stop("Argument wing_length must be defined.")
+    stop("Argument 'wing_length' must be defined.")
   } 
   if (missing(units_wing_length)) {
-    stop("Argument units_wing_length must be defined.")
+    stop("Argument 'units_wing_length' must be defined.")
   }
-  if (units_wing_length[1] == "inch") {
-    wing_length <- wing_length[1] * 25.4
+  if (units_wing_length[1] == "in") {
+    wing_length <- wing_length * 25.4
   } 
   if (units_wing_length[1] == "cm") {
-    wing_length <- wing_length[1] * 10
+    wing_length <- wing_length * 10
   }
   if (!units_wing_length[1] %in% c("cm", "mm", "inch") ) {
-    stop("Units for wing length must be either mm, cm, or inch.")
+    stop("Units for wing length must be either mm, cm, or in.")
   }
   
-  if (sex %in% c("unknown", "m", "f")) {
+  if (sex_or_strategy %in% c("unknown", "m", "f", "idi", "koi")) {
     wing_length <- log(wing_length)
-    if (sex == "unknown") {y <- exp(-2.40998 + (1.89329 * wing_length))}
-    if (sex == "m") {y <- exp(-2.48655 + (1.91227 * wing_length))}
-    if (sex == "f") {y <- exp(-2.35497 + (1.87731 * wing_length))}
+    if (sex_or_strategy == "unknown") {y <- exp(-2.40998 + (1.89329 * wing_length))}
+    if (sex_or_strategy == "m") {y <- exp(-2.48655 + (1.91227 * wing_length))}
+    if (sex_or_strategy == "f") {y <- exp(-2.35497 + (1.87731 * wing_length))}
+    if (sex_or_strategy == "idi") {y <- exp(-2.20067 + (1.81526 * wing_length))}
+    if (sex_or_strategy == "koi") {y <- exp(-2.41816 + (1.87424 * wing_length))}
   } else {
-    stop("Argument sex is not valid, see function's help.")
+    stop("Argument 'sex_or_strategy' is not valid, see function's help.")
   } 
   
   return(y)
